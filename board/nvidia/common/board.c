9720ab36787f3a310b4322814ad71c20fda6df1d
Andrey Danin <danindrey@mail.ru>
2013-07-18 17:22:54 +0400

    ARM: tegra: add nvec driver
    Adopted version of nvec driver from linux kernel with
    keyboard support only.
    
    Signed-off-by: Andrey Danin <danindrey@mail.ru>

--- board/nvidia/common/board.c
+++ board/nvidia/common/board.c
@@ -52,6 +52,9 @@
 #include <asm/arch-tegra/tegra_mmc.h>
 #include <asm/arch-tegra/mmc.h>
 #endif
+#ifdef CONFIG_TEGRA_NVEC
+#include <asm/arch-tegra/tegra_nvec.h>
+#endif
 #include <i2c.h>
 #include <spi.h>
 #include "emc.h"
@@ -216,10 +219,19 @@ int board_early_init_f(void)
 
 int board_late_init(void)
 {
+	__maybe_unused int err;
+
 #ifdef CONFIG_LCD
 	/* Make sure we finish initing the LCD */
 	tegra_lcd_check_next_stage(gd->fdt_blob, 1);
 #endif
+
+#ifdef CONFIG_TEGRA_NVEC
+	err = board_nvec_init();
+	if (err)
+		debug("NVEC controller init failed: %d\n", err);
+#endif
+
 	return 0;
 }
 
