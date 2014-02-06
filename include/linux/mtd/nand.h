77414ca81d19220f2a269690c1eee52f6ad56f77
Antony Pavlov <antonynpavlov@gmail.com>
2014-02-06 23:24:46 +0400

    initial dm365v2r (aka virt2real) commit
    This commit is based on the original commit
    from https://github.com/virt2real/v2r_uboot.git
    
        commit 4a67e5677953e4e065a7366834b022b79b5fc8f0
        Author: AV2R <avs@virt2real.com>
        Date:   Thu Jul 4 20:46:47 2013 +0400
    
            First commit of adoptet bootloader
    
    The main differences from original commit:
    
        * rebased over v2011.09 (original over v2010.12-rc2);
        * drop generated files;
        * don't drop dm365-unrelated files (original commit drops
          all MIPS, i386 etc files);
        * drop cmd_lcd_cmds;
        * move uflash tool into sepate commit;
        * enable CONFIG_SYS_{I,D,L2}CACHE_OFF;
        * drop DM646X-related stuff.
    
    This commit has to be cleaned and splitted in the future.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- include/linux/mtd/nand.h
+++ include/linux/mtd/nand.h
@@ -358,6 +358,7 @@ struct nand_ecc_ctrl {
 	int prepad;
 	int postpad;
 	struct nand_ecclayout	*layout;
+	struct nand_ecclayout	*ubl_layout;
 	void *priv;
 	void (*hwctl)(struct mtd_info *mtd, int mode);
 	int (*calculate)(struct mtd_info *mtd, const uint8_t *dat,
